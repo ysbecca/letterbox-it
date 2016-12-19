cdbf4d979c34fe779f4bc9e9822666881f7917e9
Rebecca Stone <ysbecca@Rebeccas-MacBook-Pro-4.local>
2016-12-19 21:46:08 +0000

    google map with drawing library, no saving

--- letterbox-it/app/views/distributions/_distribution.json.jbuilder
+++ letterbox-it/app/views/distributions/_distribution.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! distribution, :id, :date, :participants, :routes, :comments, :created_at, :updated_at
+json.extract! distribution, :id, :date, :participants, :quantity, :routes, :comments, :created_at, :updated_at
 json.url distribution_url(distribution, format: :json)
--- letterbox-it/db/schema.rb
+++ letterbox-it/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161211205026) do
+ActiveRecord::Schema.define(version: 20161214221210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20161211205026) do
     t.text     "comments"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "quantity"
   end
 
 end
